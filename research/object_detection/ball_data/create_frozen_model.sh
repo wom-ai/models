35da88c18fde48126c42931f492167314b3ed8b1
Jaeyoung Lee <jylee0121@gmail.com>
2020-02-27 07:42:23 +0000

    update: update scripts for ssdlite_mobilenet_v2
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- research/object_detection/ball_data/create_frozen_model.sh
+++ research/object_detection/ball_data/create_frozen_model.sh
@@ -1,10 +1,40 @@
+#!/bin/sh
+
+MODEL_NAME=ssdlite_mobilenet_v2_ball_2020_02_26
 INPUT_TYPE=image_tensor
 PIPELINE_CONFIG_PATH=./object_detection/ball_data/ssdlite_mobilenet_v2_ball.config
-TRAINED_CKPT_PREFIX=./object_detection/ball_data/model_dir/model.ckpt-300000
-EXPORT_DIR=./object_detection/ball_data/frozen_model
+TRAINED_CKPT_DIR_PATH=./object_detection/ball_data/model_dir_${MODEL_NAME}
+
+# find the latest mkpt file and get step number
+#CKPT_FILE=$(ls -t ${TRAINED_CKPT_DIR_PATH}/model.ckpt-* | head -1 | awk -F"[.-]" "{ print $3}")
+CKPT_FILE_PATH=$(ls -t ${TRAINED_CKPT_DIR_PATH}/model.ckpt-* | head -1)
+CKPT_FILE_NAME=$(basename ${CKPT_FILE_PATH})
+CKPT_STEP=$(echo "${CKPT_FILE_NAME}" | awk -F"[.-]" '{ print $3 }')
+TRAINED_CKPT_PREFIX=${TRAINED_CKPT_DIR_PATH}/model.ckpt-${CKPT_STEP}
+
+EXPORT_DIR=./object_detection/ball_data/frozen_model_${MODEL_NAME}
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "MODEL_NAME=${MODEL_NAME}"
+echo "TRAINED_CKPT_DIR_PATH=${TRAINED_CKPT_DIR_PATH}"
+echo "CKPT_FILE_PATH=${CKPT_FILE_PATH}"
+echo "CKPT_FILE_NAME=${CKPT_FILE_NAME}"
+echo "latest ckpt step is $CKPT_STEP"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+
+if [ -d ${EXPORT_DIR} ]
+then
+  rm -v ${EXPORT_DIR}/* -rf
+fi
+
 python object_detection/export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --trained_checkpoint_prefix=${TRAINED_CKPT_PREFIX} \
     --output_directory=${EXPORT_DIR} \
     --input_shape="1,-1,-1,3"
+
+cd ./object_detection/ball_data/ && \
+tar czvf frozen_model_${MODEL_NAME}.tar.gz ./frozen_model_${MODEL_NAME} && \
+cp -v frozen_model_${MODEL_NAME}.tar.gz /data/
--- research/object_detection/ball_data/train_ball_detect.sh
+++ research/object_detection/ball_data/train_ball_detect.sh
@@ -1,2 +1,2 @@
 # run this on tensorflow/models/research
-python3 ./object_detection/model_main.py --model_dir=./object_detection/ball_data/model_dir --pipeline_config_path=./object_detection/ball_data/ssdlite_mobilenet_v2_ball.config
+python3 ./object_detection/model_main.py --model_dir=./object_detection/ball_data/model_dir_ssdlite_mobilenet_v2_ball_2020_02_26 --pipeline_config_path=./object_detection/ball_data/ssdlite_mobilenet_v2_ball.config
