03e1fe9ca04e4b93db8668d2d8582c8fb443eb42
Jaeyoung Lee <jylee0121@gmail.com>
2020-06-18 23:56:35 +0000

    new: setup and scripts for ssd_mobilenet_v1
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ research/object_detection/ball_data/train_ball_detect_ssd_mobilenet_v1_ball_2020_06_11.sh
@@ -0,0 +1,2 @@
+# run this on tensorflow/models/research
+python3 ./object_detection/model_main.py --model_dir=./object_detection/ball_data/model_dir_ssd_mobilenet_v1_ball_2020_06_11 --pipeline_config_path=./object_detection/ball_data/ssd_mobilenet_v1_ball.config
