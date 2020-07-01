31419dc3cb19a6373e5c1aae2a9f5c11364656ac
Jaeyoung Lee <jylee0121@gmail.com>
2020-07-01 02:03:05 +0000

    new: setup and scripts for ssdlite_mobilenet_v1 with box predictor's kernel_size=1
     - This setup is NOT RUNNABLE because depthwise=true and kernel_size=1 are not compatible
    
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ research/object_detection/ball_data/train_ball_detect_ssdlite_mobilenet_v1_ball_kernel_1_2020_07_01.sh
@@ -0,0 +1,2 @@
+# run this on tensorflow/models/research
+python3 ./object_detection/model_main.py --model_dir=./object_detection/ball_data/model_dir_ssdlite_mobilenet_v1_ball_kernel_1_2020_07_01 --pipeline_config_path=./object_detection/ball_data/ssdlite_mobilenet_v1_ball_kernel_1.config
