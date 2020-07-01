8b3a62f8e6440ca8f70d7f46b70dcd0bcd73e7e5
Jaeyoung Lee <jylee0121@gmail.com>
2020-07-01 01:31:34 +0000

    new: setup and scripts for ssd_mobilenet_v1 with box predictor's kernel_size=1
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ research/object_detection/ball_data/train_ball_detect_ssd_mobilenet_v1_ball_kernel_1_2020_06_23.sh
@@ -0,0 +1,2 @@
+# run this on tensorflow/models/research
+python3 ./object_detection/model_main.py --model_dir=./object_detection/ball_data/model_dir_ssd_mobilenet_v1_ball_kernel_1_2020_06_23 --pipeline_config_path=./object_detection/ball_data/ssd_mobilenet_v1_ball_kernel_1.config
